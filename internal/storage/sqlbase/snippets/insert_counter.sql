1b81ea661b52a5860e95b949c91bb55eb8a62970
Sergey Vostrikov <90amper@mail.ru>
2023-07-18 22:42:52 +0300

    sv: counter logic fix

--- internal/storage/sqlbase/snippets/insert_counter.sql
+++ internal/storage/sqlbase/snippets/insert_counter.sql
@@ -2,5 +2,5 @@ insert into metmon.counters (metric_id, cvalue)
 values ((select metric_id from metmon.metrics where name = $1), $2)
 on conflict (metric_id) do
 update set
-cvalue = excluded.cvalue,
+cvalue = metmon.counters.cvalue + excluded.cvalue,
 "timestamp" = now();
