28ae1f49042b711198fd55c5e2a6f6578756d38c
Sergey Vostrikov <90amper@mail.ru>
2023-08-01 02:08:29 +0300

    sv: clear default hash key

--- internal/agent/config/config.go
+++ internal/agent/config/config.go
@@ -12,7 +12,7 @@ func init() {
 	pflag.StringVarP(&Config.ServerURL, "address", "a", "localhost:8080", "server URL")
 	pflag.IntVarP(&Config.ReportInterval, "report", "r", 10, "metrics report interval")
 	pflag.IntVarP(&Config.PollInterval, "poll", "p", 2, "metrics poll interval")
-	pflag.StringVarP(&Config.HashKey, "hash-key", "k", "$ecretKey", "hash secret key")
+	pflag.StringVarP(&Config.HashKey, "hash-key", "k", "", "hash secret key")
 	// pflag.StringVarP(&Config.HashAlg, "hash-alg", "ha", "SHA256", "hash algorithm")
 
 	pflag.Parse()
